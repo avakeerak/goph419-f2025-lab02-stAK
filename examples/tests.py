63e577e769b60e5aefe197ac60cbdafa14392018
avakeerak <ava.keerak@ucalgary.ca>
2025-10-27 09:10:23 -0600

    created main code for testing forward substitution in tests.py

--- examples/tests.py
+++ examples/tests.py
@@ -1,2 +1,29 @@
 import numpy as np
-A = np.array([[2, 0, 0], [1, 3, 0], [4, 5, 6]], dtype=float)
+
+def main():
+    test_forward_substitution()
+
+def test_forward_substitution():
+    print("Testing forward substitution...")
+    # lower traiangular coefficient matrix
+    A = np.array([
+        [2135.0, 0.0, 0.0, 0.0,]
+        [-2135.0, 5200.0, 0.0, 0.0,]
+        [0.0, -5200.0, 5796.0, 0.0,]
+        [0.0, 0.0, -5796.0, 7060.0,]])
+    
+    print("A=")
+    print(A)
+    # right-hand side vector
+
+    b = np.array([500.0, 700.0, 1000.0, 500.0])
+    print("b=")
+    print(b)
+
+    # solve  using numpy.linalg.solve()
+
+    x_exp = np.linalg.solve(A, b)
+    print(f"x expected = {x_exp}")
+
+if __name__ == "__main__": #
+    main()
